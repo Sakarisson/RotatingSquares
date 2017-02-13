2148ec8354abbff09ba3e5e992f54029f900ff95
Kristian Sakarisson <kristian@sakarisson.com>
2017-02-13 01:35:35 +0100

    Added comments to script

--- static/main.js
+++ static/main.js
@@ -1,10 +1,14 @@
 'use strict'
 
+// Canvas constants
 const canvas = {
 	height: 800,
 	width: 800
 }
 
+/**
+ * Square class. Describes how each square looks and behaves
+ */
 class Square {
 	constructor(w, h, p, rp, r) {
 		this.width = w
@@ -13,40 +17,55 @@ class Square {
 			x: p.x,
 			y: p.y
 		}
-		this.rotationPoint = {
+		this.rotationPoint = { // The local center of the square
 			x: rp.x,
 			y: rp.y
-		} || {
+		} || { // Default value is 0, 0
 			x: 0,
 			y: 0
 		}
-		this.rotation = r || 0
+		this.rotation = r || 0 // Rotation of the square in radians (around the local center). Default is 0
 	}
 
+	/**
+	 * Draw square
+	 */
 	draw() {
 		push()
 		const w = this.width
 		const h = this.height
 		const p = this.point
 		const rp = this.rotationPoint
-		translate(this.rotationPoint.x, this.rotationPoint.y)
-		rotate(this.rotation)
+		translate(this.rotationPoint.x, this.rotationPoint.y) // Translate to local center
+		rotate(this.rotation) // Rotate
 		fill('black')
-		rect(p.x - rp.x, p.y - rp.y, w, h)
+		rect(p.x - rp.x, p.y - rp.y, w, h) // Draw rectangle relative to absolute center
 		pop()
 	}
 
+	/**
+	 * Change rotation of square
+	 */
 	rotate(r) {
-		this.rotation += r
+		this.rotation += r 
 	}
 }
 
+/**
+ * Grid class. 
+ * Contains an array of squares and several functions for doing calculations on all contained squares
+ */
 class Grid {
 	constructor() {
-		this.squares = []
+		this.squares = [] // Initialize squares to an empty array
 	}
 
-	populate(n, w, d, rp) {
+	/**
+	 * Add squares to grid.
+	 * Inputs: n - the number of squares in each direction. w - the width (and height) of each square. 
+	 * d - the distance between each square. rp - the rotation point of the squares
+	 */
+	populate(n, w, d, rp) { 
 		for(let i = 0; i < n; i++) {
 			for(let j = 0; j < n; j++) {
 				this.add(new Square(w, w, { x: i * d, y: j * d }, { x: rp.x, y: rp.y}))
@@ -54,16 +73,25 @@ class Grid {
 		}
 	}
 
+	/**
+	 * Add a single square to grid
+	 */
 	add(input) {
 		this.squares.push(input)
 	}
 
+	/**
+	 * Draw all squares in grid
+	 */
 	draw() {
 		for(let i = 0; i < this.squares.length; i++) {
 			this.squares[i].draw()
 		}
 	}
 
+	/**
+	 * Rotate all squares in grid
+	 */
 	rotate(r) {
 		for(let i = 0; i < this.squares.length; i++) {
 			this.squares[i].rotate(r)
